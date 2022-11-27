468ce4b91270afbd4b74017148516f9699950c5d
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-27 19:43:23 +0600

    mongo db server added and products uploaded

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
@@ -8,20 +9,42 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const categories = require('./data/categories.json');
-const products = require('./data/products.json')
+const categories = require("./data/categories.json");
+const products = require("./data/products.json");
 
-app.get("/categories", (req, res) => {
-  console.log('hello rakib');
-  res.send(categories);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ct9it9z.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.get("/category/:id", (req, res)=>{
-  const id = req.params.id;
-  console.log("rakib");
-  const product = products.filter(prd => prd.sub_category==id);
-  res.send(product);
-})
+async function run() {
+  try {
+    const categoryCollection = client.db("resalePort").collection("categories");
+    const productCollection = client.db("resalePort").collection("products");
+
+    app.get("/categories", async(req, res) => {
+      const query = {};
+      const categories = await categoryCollection.find(query).toArray();
+      res.send(categories);
+    });
+
+    app.get("/category/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {
+        sub_category:id
+      };
+      const product = await productCollection.find(query).toArray();
+      res.send(product);
+    });
+
+  } finally {
+  }
+}
+run().catch((err) => console.log("Error: ", err));
+
+
 
 app.listen(port, () => {
   console.log(`mongodb db server is running, ${port}`);
