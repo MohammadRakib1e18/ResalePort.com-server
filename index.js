e22200936e146629cff1cad865b5ac48607a964d
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-11-29 08:46:41 +0600

    put method for updating advertised my products

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
@@ -24,55 +24,92 @@ async function run() {
     const categoryCollection = client.db("resalePort").collection("categories");
     const productCollection = client.db("resalePort").collection("products");
     const userCollection = client.db("resalePort").collection("users");
+    const advertisedCollection = client.db("resalePort").collection("advertised");
 
-    app.get("/categories", async(req, res) => {
+    app.get("/categories", async (req, res) => {
       const query = {};
       const categories = await categoryCollection.find(query).toArray();
       res.send(categories);
     });
 
-    app.get('/singleCategory', async(req, res)=>{
+    app.get("/singleCategory", async (req, res) => {
       const category = req.query.category;
-      const query = { category_name : category};
+      const query = { category_name: category };
       const result = await categoryCollection.findOne(query);
       res.send(result);
-    })
+    });
 
-    app.get("/category/:id", async(req, res) => {
+    app.get("/category/:id", async (req, res) => {
       const id = req.params.id;
       const query = {
-        sub_category:id
+        sub_category: id,
       };
       const product = await productCollection.find(query).toArray();
       res.send(product);
     });
 
-    app.get("/user", async(req, res)=>{
+    app.get("/user", async (req, res) => {
       const email = req.query.email;
-      const query={email:email};
+      const query = { email: email };
       const result = await userCollection.findOne(query);
       res.send(result);
     });
 
-    app.post('/product', async(req, res)=>{
+    app.get("/myProducts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
+
+    app.put("/myProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updatedStatus = {
+        $set: {
+          advertised: true,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updatedStatus,
+        option
+      );
+      console.log(result);
+      res.send(result);
+    });
+
+    //advertisedProducts
+    app.get("/advertisedProducts", async (req, res) => {
+      const query = {};
+      const products = await advertisedCollection.find(query).toArray();
+      res.send(products);
+    });
+
+    app.post("/advertisedProducts", async(req, res)=>{
+      const product = req.body;
+      const result = await advertisedCollection.insertOne(product);
+      res.send(result);
+    });
+
+
+    app.post("/product", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
       res.send(result);
-    })
+    });
 
-    app.post('/users', async(req, res)=>{
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query={email:user.email};
+      const query = { email: user.email };
       const alreadyUser = await userCollection.findOne(query);
-      if(alreadyUser?.email === user?.email){
-        res.send({message:'already a registered user'})
+      if (alreadyUser?.email === user?.email) {
+        res.send({ message: "already a registered user" });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
-
-    
-
+    });
   } finally {
   }
 }
